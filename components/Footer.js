2b82513819a30b18718ba76a896a54dee9382f98
Manik Rana <manikrana54@gmail.com>
2022-03-03 12:52:23 -0500

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import footerStyles from '../styles/Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    )
+  }
+  
+  export default Footer
+  
---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+import navStyles from '../styles/Nav.module.css'
+
+export const Nav = () => {
+    return (
+        <div>
+            <h1>Nav</h1>
+        </div>
+    )
+  }
+  
+  export default Nav
+  
